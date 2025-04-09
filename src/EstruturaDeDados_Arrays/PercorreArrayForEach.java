fc64be0dd3c0f519d0470d7f9ea29bfca742ada0
diegopessoa <diegopessoa160@gmail.com>
2025-04-09 00:00:55 -0400

    Usando foreach para percorre a Arrays

---
+++ src/EstruturaDeDados_Arrays/PercorreArrayForEach.java
@@ -0,0 +1,12 @@
+package EstruturaDeDados_Arrays;
+
+public class PercorreArrayForEach {
+    public static void main(String[] args) {
+
+        int [] numeros = {1,2,3,4,5,6,7};
+
+        for (int num : numeros){
+            System.out.print (num +" ");
+        }
+    }
+}
