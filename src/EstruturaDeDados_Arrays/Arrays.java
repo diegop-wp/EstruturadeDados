0bdb7dd90b05f34ac7af7c2787788b44bf61ff4e
diegopessoa <diegopessoa160@gmail.com>
2025-04-08 23:37:09 -0400

    criando uma arrays

---
+++ src/EstruturaDeDados_Arrays/Arrays.java
@@ -0,0 +1,15 @@
+package EstruturaDeDados_Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // criando uma arrays
+        int [] idades = {12,43,54};
+
+        // imprimindo valores da arrays
+
+        System.out.println("idade1: "+ idades[0]);
+        System.out.println("idade2: "+idades[1]);
+        System.out.println("idade3: "+idades[2]);
+    }
+}
