3ebbbb2bf55bff3b725f179b93ad9233a360fa34
diegopessoa <diegopessoa160@gmail.com>
2025-07-18 00:10:15 -0400

    up

---
+++ src/ordenacao/Insertion_Sort.java
@@ -0,0 +1,35 @@
+package ordenacao;
+
+public class Insertion_Sort {
+    public static void main(String[] args) {
+
+        int[] vetor = new int[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random() * vetor.length);
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+
+        int aux, j;
+        for (int i = 1; i < vetor.length; i++) {
+            aux = vetor[i];
+            j = i - 1;
+
+            while (j >= 0 && vetor[j] > aux){
+                 vetor[j+1] = vetor[j];
+                 j--;
+            }
+
+            vetor[j+1] = aux;
+        }
+        System.out.println("-------- ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]+ " ");
+        }
+
+    }
+
+}
