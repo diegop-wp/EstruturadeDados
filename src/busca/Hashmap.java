c16e7eeb0b95c525ac0b2c228a46bd43b1290eb6
diegopessoa <diegopessoa160@gmail.com>
2025-07-28 20:32:07 -0400

    up

---
+++ src/busca/Hashmap.java
@@ -0,0 +1,29 @@
+package busca;
+
+import busca.ex.Produto;
+
+import java.util.HashMap;
+
+public class Hashmap {
+    private static final int TAMANHO= 1000000;
+
+    public static void main(String[] args) {
+
+        HashMap <Integer, Produto> map = new HashMap<Integer,Produto>();
+
+        for (int i = 0; i < TAMANHO; i++) {
+            map.put(i, new Produto(i, "produto "+i, i*20.00));
+        }
+
+        long starttime = System.currentTimeMillis();
+        int codigoSerEncontrado = TAMANHO;
+
+        for (int qtd = 0; qtd < TAMANHO; qtd++) {
+            Produto p = map.get(codigoSerEncontrado);
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Tempo de Execução: "+(endTime - starttime)+ "ms");
+    }
+}
